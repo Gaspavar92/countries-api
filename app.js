98a856fe35cdcd6de19158e6fff807222996c15b
Gaspare Tortora <g.tortora2@gmail.com>
2023-05-26 13:14:48 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import { app } from "./firebase.js";
+import {getDatabase, ref, onValue, get, push, update, set} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+
+const database = getDatabase(app);
+const dbRef = ref(database);
+
+onValue(dbRef, (data) => {
+    if (data.exists()) {
+        for (let key in data.val()) {
+            console.log(data.val()[key].name)
+        }
+    } else {
+        console.log('Nothing here');
+    }
+})
+
+
---
+++ firebase.js
@@ -0,0 +1,31 @@
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  export const firebaseConfig = {
+
+    apiKey: "AIzaSyCoD_VHQfVR_BBQ0FiUIAOw2aXJ-RIEOlM",
+
+    authDomain: "flags-fem.firebaseapp.com",
+
+    projectId: "flags-fem",
+
+    storageBucket: "flags-fem.appspot.com",
+
+    messagingSenderId: "423287024862",
+
+    appId: "1:423287024862:web:55ff35a79c8fe04580a64f"
+
+  };
+
+
+  // Initialize Firebase
+
+  export const app = initializeApp(firebaseConfig);
