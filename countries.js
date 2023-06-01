18658b9e360a8c68666168e1249da6b40a24e6f0
Gaspare Tortora <g.tortora2@gmail.com>
2023-06-01 12:03:16 -0400

    Improved function for HTML to append

--- countries.js
+++ countries.js
@@ -1,12 +1,13 @@
-export function countryHTML(country, key) {
+export function countryHTML(data, key) {
+    const country = data[key];
  return   `
 <div class="country">
-<img src="${country[key].flags.png}" alt="${country[key].name.common} flag" id="country-flag">
-<div class="country-name">${country[key].name.common}${country[key].name.official === country[key].name.common ? "" : `, also known as ${country[key].name.official}`}</div>
+<img src="${country.flags.png}" alt="${country.name.common} flag" id="country-flag">
+<div class="country-name">${country.name.common}${country.name.official === country.name.common ? "" : `, also known as ${country.name.official}`}</div>
 <div class="country-info">
-    <p class="population">Population: ${country[key].population.toLocaleString("en-US")}</p>
-    <p class="region">Region: ${country[key].region}</p>
-    <p class="capital">Capital: ${country[key].capital === undefined ? "N/A" : country[key].capital}</p>
+    <p class="population">Population: ${country.population.toLocaleString("en-US")}</p>
+    <p class="region">Region: ${country.region}</p>
+    <p class="capital">Capital: ${country.capital === undefined ? "N/A" : country.capital}</p>
 </div>
 `
 }
---
+++ item.js
@@ -0,0 +1,11 @@
+// Select dom element to expand
+
+// setTimeout(() => {
+//     const country = document.querySelector('.country');
+//     console.log(country)
+// }, 1000);
+
+
+// Change CSS in order to display element at the center in a fixed position
+// Add dynamically all additional elements to display
+// Change styling to fit current configuration
