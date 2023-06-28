2628921911f912797ca4a924017b679208f0e71b
Gaspare Tortora <g.tortora2@gmail.com>
2023-06-27 21:32:14 -0400

    Deleted unnecessary code

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-// import { app } from "./firebase.js";
-// import {getDatabase, ref, onValue, get, push, update, set} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
-
-// const database = getDatabase(app);
-// const dbRef = ref(database);
-
-// onValue(dbRef, (data) => {
-//     if (data.exists()) {
-//         for (let key in data.val()) {
-//             console.log(data.val()[key].name)
-//         }
-//     } else {
-//         console.log('Nothing here');
-//     }
-// })
-
 import { getCountries } from "./api.js";
 import { displayCountries } from "./display.js";
 import { filterCountries } from "./filter.js";
